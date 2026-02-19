a50cb3668e7560aeee669b7969cd817cf141fae5
gilsoftware08 <gilsoftware08@gmail.com>
2026-02-19 11:42:38 +0530

    Removed deprecated bundledWebRuntime from config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sols.app',
+  appId: 'com.your.appid', // (whatever yours is)
   appName: 'solsapp',
   webDir: 'out',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https'
   }
